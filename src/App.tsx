204b0bfcc2f8221025132b3bf8bcd34848d32bdf
suzz-in <o_o_su_jin_@naver.com>
2023-04-26 14:00:50 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="w-10 h-10 bg-green-200">메인</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <nav>헤더</nav>;
+}
+
+export default Header;
