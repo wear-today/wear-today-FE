64b02280351cb6b1f2f330e9dea5be7ac3b43d8a
hyunjung <sspp4555@naver.com>
2023-05-11 19:16:36 +0900

    cute

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -1,19 +1,40 @@
+
 import { useEffect } from 'react';
+import axios from 'axios';
 
 function WeatherData() {
-  const get = async () => {
+  const get = async (position) => {
+    const today = new Date();
+      const lat = position&&position.coords.latitude;
+    const lon = position&&position.coords.longitude;
+        console.log(lat,'lat')
+    const base_date = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate()-1;
+    const base_time = today.getHours() < 2 ? '23' : '05';
+  console.log(base_date,'bd')
+  console.log(base_time,'bd')
     const apikey = import.meta.env.VITE_WEATHER_API_KEY;
-    const today = 20230509;
-    const api = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0`;
-    const getdata = await fetch(
-      `${api}/getVilageFcst?serviceKey=${apikey}pageNo=1dataType=JSON&base_date=${today}&base_time=0500&nx=55&ny=127`,
-    ).then(console.log);
-
-    return getdata;
+    //const url = `http://apis.data.go.kr/1360000/AsosHourlyInfoService/getWthrDataList?serviceKey=${apikey}&numOfRows=10&pageNo=1&dataCd=ASOS&dateCd=HR&stnIds=108&endDt=20200310&endHh=01&startHh=01&startDt=20190120`;
+    const url = `https://apis.data.go.kr/1360000/AsosHourlyInfoService/getWthrDataList?serviceKey=${apikey}&pageNo=1&numOfRows=10&dataType=json&dataCd=ASOS&dateCd=HR&startDt=${base_date}&startHh=${base_time}&endDt=${base_date}&endHh=${base_time}&stnIds=108`
+                                                                                                            //pageNo=1&numOfRows=10&dataType=XML&dataCd=ASOS&dateCd=HR&startDt=20220510&startHh=01&endDt=20220510&endHh=01&stnIds=108
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+      //const realdata = JSON.parse(data)
+      console.log(data, 'data');
+      //console.log(realdata, 'realdata');
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  function geterror() {
+    alert('위치한 곳을 찾을 수 없습니다.');
+  }
+
   useEffect(() => {
-    get();
+    navigator.geolocation.getCurrentPosition(get, geterror);
   }, []);
+
   return <></>;
 }
 
